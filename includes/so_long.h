fea250e17b51b3a3a9cd4140fd54c8a8ff78596a
Cristopher Tommasi <ctommasi@student.42madrid.com>
2024-11-08 14:52:50 +0100

    fixed norminette issues
    Fixed DFS algorithm

--- includes/so_long.h
+++ includes/so_long.h
@@ -19,23 +19,23 @@
 
 typedef struct s_data
 {
-	mlx_t	*mlx_ptr;
+	mlx_t		*mlx_ptr;
 	mlx_image_t	*img;
-	char	*window_name;
+	char		*window_name;
 	int			window_w;
 	int			window_h;
 	int			image_w;
 	int			image_h;
-	char	*pre_map;
-	char	**map;
+	char		*pre_map;
+	char		**map;
 	int			rows;
 	int			columns;
-	char	**p_pos;
+	char		**p_pos;
 	int			p_row;
 	int			p_col;
 	mlx_image_t	*p_image;
 	int			moves;
-	int	**visited;
+	int			**visited;
 	int			start_row;
 	int			start_col;
 	int			n_collectibles;
@@ -43,23 +43,23 @@ typedef struct s_data
 	int			found_exit;
 }	t_data;
 
-int	main(int argc, char **argv);
-void		error(t_data *data, char c);
-void		free_data(t_data *data);
-void		free_fd(char *line, int *fd);
-void    	init_struct(t_data *data);
-void		check_ber_file(t_data *data, char **argv);
-void		check_map_rectangle(t_data *data, char **argv);
-void		read_map(t_data *data, char **argv);
-void    	check_walls(t_data *data);
-void    	check_map_dupes(t_data *data);
-void		check_map_doable(t_data *data);
-void		init_window(t_data *data);
-void		render_background(t_data *data);
-void		get_player_pos(t_data *data);
-void		render_player(t_data *data, int p_row, int p_col);
-void		on_keypress(struct mlx_key_data keydata, void *param);
-void		hook_and_loop(t_data *data);
-void		on_destroy(void *data);
+int		main(int argc, char **argv);
+void	error(t_data *data, char c);
+void	free_data(t_data *data);
+void	free_fd(char *line, int *fd);
+void	init_struct(t_data *data);
+void	check_ber_file(t_data *data, char **argv);
+void	check_map_rectangle(t_data *data, char **argv);
+void	read_map(t_data *data, char **argv);
+void	check_walls(t_data *data);
+void	check_map_dupes(t_data *data);
+void	check_map_doable(t_data *data);
+void	init_window(t_data *data);
+void	render_background(t_data *data);
+void	get_player_pos(t_data *data);
+void	render_player(t_data *data, int p_row, int p_col);
+void	on_keypress(struct mlx_key_data keydata, void *param);
+void	hook_and_loop(t_data *data);
+void	on_destroy(void *data);
 
 #endif
--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -51,7 +51,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-long		ft_atoi(const char *nptr);
+long	ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
